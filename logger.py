485ae81589d1bef8ae81f05f4afe0a74aa7f0ec4
evgeniy gazetdinov <zplacesound@gmail.com>
2019-07-18 16:18:07 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,15 @@
 import logging
+import sys
 logging.basicConfig(filename = "reports.log",level = logging.DEBUG,format='%(asctime)s %(message)s')
 logging.info("this info")
 logging.warning("this warning")
 logging.debug("this debug")
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
