e541d672e45a1438fb98813253d61fffc1f8bafc
evgeniy gazetdinov <zplacesound@gmail.com>
2019-07-19 17:30:32 +0300

    Add files via upload

---
+++ twist_exp.py
@@ -0,0 +1,12 @@
+import sys
+from twisted.web.static import File
+from twisted.python import log
+from twisted.web.server import Site
+from twisted.internet import reactor
+import os
+
+log.startLogging(sys.stdout)
+root = File('~')
+site = Site(root)
+reactor.listenTCP(8080, site)
+reactor.run()
