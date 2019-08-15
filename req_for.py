eda6266fea71abe0b7747491630f2bf985e927a9
evgeniy gazetdinov <zplacesound@gmail.com>
2019-08-15 06:57:19 +0300

    Update req_for.py

--- req_for.py
+++ req_for.py
@@ -4,6 +4,9 @@ import requests
 
 def main(link):
     response = requests.get()
+    if response.status_code != 200:
+        print("Something wrong, status code, "response.status_code)
+        raise Exception("error!"*5)
     print("Status ",response.status_code)
     print("Content ",response['content type'])
     data = response.json()
