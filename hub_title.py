cc7d7eb228a4ff9c4b81cd2cd88015629d8b2a8a
evgeniy gazetdinov <zplacesound@gmail.com>
2019-09-04 19:26:57 +0300

    worked proto

--- hub_title.py
+++ hub_title.py
@@ -1,21 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import random
 
 
 def find_titles():
     page = requests.get("https://rt.pornhub.com/")
     soup = BeautifulSoup(page.content,'html.parser')
     actual = (soup.find_all("span",class_ = 'title'))
-    return actual
+    titles = []
+    for i in range(len(actual)):
+        titles.append(actual[i].get_text())
+    return titles
 
-#def to_json(course,changes):
-#    rates = json.dumps({'base':'USD','target':'RUB','price':course,'changes':changes})
-#    return rates
+def to_json(titles):
+    secure_random = random.SystemRandom()
+    rates = json.dumps({'random_title':secure_random.choices(titles)})
+    return rates
 
 
 if __name__ == "__main__":
  #   rates = to_json(rub,changes)
   #  print(rates)
     titles = find_titles()
-    print(titles)
+    random_json = to_json(titles)
+    print(random_json)
