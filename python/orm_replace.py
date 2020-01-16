02decdf88c7bf3c513f6e7e61919966b9418eb34
evgeniy gazetdinov <zplacesound@gmail.com>
2020-01-16 12:45:07 +0300

    Update orm_replace.py

--- python/orm_replace.py
+++ python/orm_replace.py
@@ -1,1 +1,3 @@
+from django.db.models import F, Func, Value
+from myapp.models import News
 News.objects.filter(long_description__icontains = '<br>').update(long_description=Func(F('long_description'),Value('<br>'),Value('p'),function='replace'))
