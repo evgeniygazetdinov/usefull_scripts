2c9cf9d08f2af6c3ec9cb73b1f406b22e528767f
evgeniy gazetdinov <zplacesound@gmail.com>
2020-01-16 13:01:08 +0300

    Update orm_replace.py

--- python/orm_replace.py
+++ python/orm_replace.py
@@ -1,3 +1,5 @@
 from django.db.models import F, Func, Value
-from myapp.models import News
-News.objects.filter(long_description__icontains = '<br>').update(long_description=Func(F('long_description'),Value('<br>'),Value('p'),function='replace'))
+from apps.news.models import News
+News.objects.filter(long_description__icontains = '<center>').update(long_description=Func(F('long_description'),Value('<center>'),Value('<div style="text-align: center">'),function='replace'))
+News.objects.filter(long_description__icontains = '</center>').update(long_description=Func(F('long_description'),Value('</center>'),Value('</div'>),function='replace')) 
+News.objects.filter(long_description__icontains = '<br>').update(long_description=Func(F('long_description'),Value('<br>'),Value('p'),function='replace'))   
