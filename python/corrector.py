920a9ff13158424ef96cc0a8dcdd8ef2d970b16f
Evgeniy Gazetdinov <zplacesound@gmail.com>
2021-10-20 10:52:25 +0300

    Update corrector.py
    add worked example

--- python/corrector.py
+++ python/corrector.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import subprocess
+
+pylint = "pylint"
+mypy = "mypy"
+flake8 = "flake8"
+
 
 def get_current_terminal_language():
     """
@@ -14,22 +20,28 @@ def get_current_terminal_language():
     }
     output = subprocess.getoutput("echo $LANG")
     return data[output]
-
+  
 def python_file_finder():
     lines  = os.popen('git status | grep -i {}'.format(get_current_terminal_language())).readlines()
     # add check on py
     data = [line.split()[-1] for line in lines]
-    print(data)
+    return data
 
 def check_on(corrector, file):
-  return ""
+    output = subprocess.getoutput("{} {}".format(corrector, file))
+    print(output)
 
+    
 def main():
   files = python_file_finder()
-#  map(check_on, 'pylint', files) # pylint
-#  map(check_on, 'flake8', files) # flake8
-#  map(check_on, 'mypy', files) # mypy
+  print("*"*10 + pylint +"*"*10)
+  [check_on(pylint, file) for file in files if file is not None]
+  print("*"*10 + flake8 +"*"*10)
+  [check_on(flake8, file) for file in files if file is not None]
+  print("*"*10 + mypy +"*"*10)
+  [check_on(mypy, file) for file in files if file is not None]
+  print("*"*10 + "-"*5 + "END" +"-"  +"*"*10)
 
 if __name__ == "__main__":
-   main()
+    main()
 
