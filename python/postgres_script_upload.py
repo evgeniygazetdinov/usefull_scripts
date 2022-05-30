0489599ae56f49b6d116b6bce6a5fe78712bd18e
Evgeniy Gazetdinov <zplacesound@gmail.com>
2022-05-30 19:43:10 +0300

    Update postgres_script_upload.py

--- python/postgres_script_upload.py
+++ python/postgres_script_upload.py
@@ -1,11 +1,23 @@
 
 import os
 import sys
+import shutil
 
-os.system('''sudo docker exec -it billing_postgres psql -U billing -c "DROP DATABASE billing_prod3"''')
-print('database removed')
-os.system('''sudo docker exec -it billing_postgres psql -U billing -c "create DATABASE billing_prod3 with owner billing"''')
-print('new database created')
-print('begin upload dump')
-os.system('''cat billing_prod_20225196528_5401.sql | docker exec -i billing_postgres psql -U billing -d billing_prod3''')
-print('upload over')
+layer_path = '/home/evgesha/Documents'
+layer_name = 'Individualnyy_p=210303_0eiBi85-MYSHOP.FDB'
+def db_operations():
+	os.system('''sudo docker exec -it billing_postgres psql -U billing -c "DROP DATABASE billing_prod3"''')
+	print('database removed')
+	os.system('''sudo docker exec -it billing_postgres psql -U billing -c "create DATABASE billing_prod3 with owner billing"''')
+	print('new database created')
+	print('begin upload dump')
+	os.system('''cat billing_prod_20225196528_5401.sql | docker exec -i billing_postgres psql -U billing -d billing_prod3''')
+	print('upload over')
+def move_layer():
+	os.chdir(layer_path)
+	shutil.copy2(layer_path+ '/'+ layer_name  , '/home/evgesha/code/storecraft/.deploy/data/firebird/storage/')
+
+
+if __name__ == '__main__':
+	db_operations()
+	move_layer()
