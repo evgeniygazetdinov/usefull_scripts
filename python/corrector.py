8b8a63e3605e06a779b30aa135fb681b1fd2f680
Evgeniy Gazetdinov <zplacesound@gmail.com>
2022-08-01 18:18:27 +0300

    Update corrector.py

--- python/corrector.py
+++ python/corrector.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 import os
 import subprocess
+import sys
 
 pylint = "pylint"
 mypy = "mypy"
@@ -20,27 +21,47 @@ def get_current_terminal_language():
     output = subprocess.getoutput("echo $LANG")
     return data[output]
   
+def is_python_file(file):
+    if file.endswith('.py'):
+        return True
+    return False
+
 def python_file_finder():
-    lines  = os.popen('git status | grep -i {}'.format(get_current_terminal_language())).readlines()
+    lines  = os.popen('git status | grep -i изменено').readlines()
     # add check on py
-    data = [line.split()[-1] for line in lines]
-    return data
+    data =  [(line.split(' ')[-1]).split('\n') for line in lines]
+    
+    without_n = [line[0] for line in data]
+    print(without_n)
+    return without_n
 
 def check_on(corrector, file):
     output = subprocess.getoutput("{} {}".format(corrector, file))
     print(output)
 
+def correct_runner(files):
+   for file in files:
+      if file is not None:
+    
+        print("\t")
+        print('*'*10,file,'*'*10) 
+        print("*"*10 + pylint +"*"*10)
+        check_on('pylint', file)
+        print("*"*10 + flake8 +"*"*10)
+        check_on('flake8', file)
+        print("*"*10 + mypy +"*"*10)
+        check_on('mypy', file)
+        print("*"*10 + "-"*5 + "END" +"-"  +"*"*10)
+        print("\t")
+
 def main():
-  files = python_file_finder()
-  print("*"*10 + pylint +"*"*10)
-  [check_on(pylint, file) for file in files if file is not None]
-  print("*"*10 + flake8 +"*"*10)
-  [check_on(flake8, file) for file in files if file is not None]
-  print("*"*10 + mypy +"*"*10)
-  [check_on(mypy, file) for file in files if file is not None]
-  print("*"*10 + "-"*5 + "END" +"-"  +"*"*10)
+  if len(sys.argv) == 2:
+     print(sys.argv)
+     correct_runner([sys.argv[1]])    
+  else:
+     files = python_file_finder()
+     correct_runner(files)
 
 if __name__ == "__main__":
     main()
-
-
+    
